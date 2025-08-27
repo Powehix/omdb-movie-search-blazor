589288486cf98732864469755a4a35a57b9565d7
mezha <mezhalee@gmail.com>
2025-08-27 11:10:41 +0300

    Add Radzen and sample components to SearchPage

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,8 @@
-using OMDbMovieSearch.Client;
-using OMDbMovieSearch.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OMDbMovieSearch.Client;
+using OMDbMovieSearch.Client.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<MovieApiService>();
+
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
